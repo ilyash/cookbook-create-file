e6a381713cbe7dd60d4e2cd7cf8a4bab2492947e
Ilya Sher <ilya.sher@coding-knight.com>
2013-10-15 13:43:03 +0300

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+Description
+===========
+Creates `/tmp/chef-works` file. Created for testing Chef operation.
+
+Requirements
+============
+
+Attributes
+==========
+
+Usage
+=====
+`knife bootstrap 127.0.0.1 -N some_host_name -p 2222 -r 'recipe[create-file]'`
---
+++ recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: create-file
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+f="/tmp/chef-works"
+
+Chef::Log.warn("Will create file #{f}")
+
+file f do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  content "yes!"
+end
